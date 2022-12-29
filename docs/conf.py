033f20b7163b05ed23b5adf2562874eacc4db276
Shreya Rajpal <shreya.rajpal@gmail.com>
2022-12-29 20:50:43 +0530

    getting started guide and key concepts

--- docs/conf.py
+++ docs/conf.py
@@ -89,3 +89,5 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path: list = []
+
+myst_enable_extensions = ["colon_fence"]
